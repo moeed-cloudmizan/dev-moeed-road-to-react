4a7c22048479217a54f279776c7736cb073dc149
moeed-cloudmizan <moeed.ahmad@cloudmizan.com>
2024-07-03 15:51:32 +0500

    First 10 to-do's done

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+const title = 'React';
+const welcome = {
+  greeting: 'Hey',
+  title: 'React'
+};
+
+const list = [
+  {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+  ];
+function Search()
+{
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  )
+}
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello {title}
+      </h1>
+      <h2>
+        {welcome.greeting} {welcome.title} 
+      </h2>
+      <Search></Search>
+      <List></List>
+      
+    </div>
+  );
+}
+function List(){
+  return (
+    <ul>
+        {list.map(function (item) {
+        return <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}:</a>
+        </span>
+        <span> Author: {item.author}, 
+          </span>
+          <span> Num of Comments: {item.num_comments}, 
+            </span>
+            <span> Points: {item.points} 
+            </span></li>;
+        })}
+      </ul>
+  );
+}
+export default App; 
